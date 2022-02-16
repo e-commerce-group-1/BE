52080b3c32111fe08ca2e0d48c4d8c5b6aa84def
fransihsan <fransihsan8888@gmail.com>
2022-02-16 08:49:19 +0700

    Initial fitur-login

---
+++ repository/auth/auth.go
@@ -0,0 +1,27 @@
+package auth
+
+import (
+	u "group-project1/enitities/user"
+
+	"gorm.io/gorm"
+)
+
+type AuthRepository struct {
+	db *gorm.DB
+}
+
+func New(db *gorm.DB) *AuthRepository {
+	return &AuthRepository{
+		db: db,
+	}
+}
+// =========================== Login =================================
+func (a *AuthRepository) Login(email, password string) (u.User, error) {
+	loggedInUser := u.User{Email: email, Password: password}
+
+	if err := a.db.First(&loggedInUser).Error; err != nil {
+		return loggedInUser, err
+	}
+	return loggedInUser, nil
+}
+// ===================================================================
