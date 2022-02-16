54a66e7f20bdaf0d14cbffe480f85e89dfb85396
fransihsan <fransihsan8888@gmail.com>
2022-02-16 10:38:22 +0700

    Initial Payment Method

---
+++ repository/payment_method/payment_method.go
@@ -0,0 +1,63 @@
+package payment_method
+
+import (
+	pay "group-project1/enitities/payment_method"
+
+	"github.com/labstack/gommon/log"
+	"gorm.io/gorm"
+)
+
+type PaymentMethodRepository struct {
+	db *gorm.DB
+}
+
+func New(db *gorm.DB) *PaymentMethodRepository {
+	return &PaymentMethodRepository{db: db}
+}
+
+// ======================== Insert Payment Method ================================
+func (ur *PaymentMethodRepository) Insert(newPayMethod pay.Payment_Method) (pay.Payment_Method, error) {
+	if err := ur.db.Save(&newPayMethod).Error; err != nil {
+		log.Warn("Found database error:", err)
+		return newPayMethod, err
+	}
+
+	return newPayMethod, nil
+}
+
+// ======================== Get Payment Methods ==================================
+func (ur *PaymentMethodRepository) Get() ([]pay.Payment_Method, error) {
+	payment_methods := []pay.Payment_Method{}
+	if err := ur.db.Find(&payment_methods).Error; err != nil {
+		log.Warn("Found database error:", err)
+		return nil, err
+	}
+	return payment_methods, nil
+}
+
+// ======================== Update Payment Method ===============================
+func (ur *PaymentMethodRepository) Update(payment_methodId int, newPayMethod pay.Payment_Method) (pay.Payment_Method, error) {
+
+	var payment_method pay.Payment_Method
+	ur.db.First(&payment_method, payment_methodId)
+
+	if err := ur.db.Model(&payment_method).Updates(&newPayMethod).Error; err != nil {
+		return payment_method, err
+	}
+
+	return payment_method, nil
+}
+
+// ======================== Delete Payment Method ===============================
+func (ur *PaymentMethodRepository) Delete(payment_methodId int) error {
+
+	var payment_method pay.Payment_Method
+
+	if err := ur.db.First(&payment_method, payment_methodId).Error; err != nil {
+		return err
+	}
+	ur.db.Delete(&payment_method, payment_methodId)
+	return nil
+
+}
+// ============================================================================
