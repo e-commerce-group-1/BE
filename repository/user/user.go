e84deef10caf3eb2119ab0293b0ddaa28904c63d
fransihsan <fransihsan8888@gmail.com>
2022-02-16 09:59:59 +0700

    Initial user

--- repository/user/user.go
+++ repository/user/user.go
@@ -24,4 +24,40 @@ func (ur *UserRepository) Insert(newUser u.User) (u.User, error) {
 
 	return newUser, nil
 }
+
+// ======================== Get Users ==================================
+func (ur *UserRepository) Get() ([]u.User, error) {
+	users := []u.User{}
+	if err := ur.db.Find(&users).Error; err != nil {
+		log.Warn("Found database error:", err)
+		return nil, err
+	}
+	return users, nil
+}
+
+// ======================== Update User ==================================
+func (ur *UserRepository) Update(userId int, newUser u.User) (u.User, error) {
+
+	var user u.User
+	ur.db.First(&user, userId)
+
+	if err := ur.db.Model(&user).Updates(&newUser).Error; err != nil {
+		return user, err
+	}
+
+	return user, nil
+}
+
+// ======================== Delete User ==================================
+func (ur *UserRepository) Delete(userId int) error {
+
+	var user u.User
+
+	if err := ur.db.First(&user, userId).Error; err != nil {
+		return err
+	}
+	ur.db.Delete(&user, userId)
+	return nil
+
+}
 // ============================================================================
