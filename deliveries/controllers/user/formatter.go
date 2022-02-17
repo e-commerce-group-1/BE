e5c5aa768c6c9745ec53aebf5d69b0afdbd0f165
ynwahid <yusufnurwahid.contact@gmail.com>
2022-02-17 21:44:33 +0700

    delete unused formatter

--- deliveries/controllers/user/formatter.go
+++ deliveries/controllers/user/formatter.go
@@ -4,46 +4,23 @@ import "group-project1/entities/user"
 
 // =================== Create User =======================
 type CreateUserRequestFormat struct {
-	Name string `json:"name" form:"name"`
-	UserName string `json:"username" form:"username"`
-	Email string `json:"email" form:"email"`
+	Name     string `json:"name" form:"name"`
+	UserName string `json:"user_name" form:"user_name"`
+	Email    string `json:"email" form:"email"`
 	Password string `json:"password" form:"password"`
 }
 
 type CreateUserResponseFormat struct {
-	Code int `json:"code"`
-	Success bool `json:"success"`
-	Message string        `json:"message"`
+	Code    int        `json:"code"`
+	Success bool       `json:"success"`
+	Message string     `json:"message"`
 	Data    user.Users `json:"data"`
-}
-
-// =================== Get Users =======================
-type GetUsersResponseFormat struct {
-	Code int `json:"code"`
-	Success bool `json:"success"`
-	Message string        `json:"message"`
-	Data   []user.Users `json:"data"`
 }
 
 // =================== Update User =======================
 type UpdateUserRequestFormat struct {
-	Name string `json:"name" form:"name"`
+	Name     string `json:"name" form:"name"`
 	UserName string `json:"username" form:"username"`
-	Email string `json:"email" form:"email"`
+	Email    string `json:"email" form:"email"`
 	Password string `json:"password" form:"password"`
-}
-
-type UpdateUserResponseFormat struct {
-	Code int `json:"code"`
-	Success bool `json:"success"`
-	Message string        `json:"message"`
-	Data    user.Users `json:"data"`
-}
-
-// =================== Delete User =======================
-type DeleteUserResponseFormat struct {
-	Code int `json:"code"`
-	Success bool `json:"success"`
-	Message string        `json:"message"`
-	Data   []user.Users `json:"data"`
 }
