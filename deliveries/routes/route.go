54d33bd21c5595af93113b84958d54fb9f384b09
ynwahid <yusufnurwahid.contact@gmail.com>
2022-02-17 21:59:39 +0700

    fix routes

--- deliveries/routes/route.go
+++ deliveries/routes/route.go
@@ -1,29 +1,31 @@
 package route
 
 import (
-	"group-project1/configs"
+	"group-project1/deliveries/controllers/address"
 	"group-project1/deliveries/controllers/auth"
 	"group-project1/deliveries/controllers/user"
-	"group-project1/deliveries/controllers/address"
+	"group-project1/deliveries/middlewares"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
 
 func RegisterPath(e *echo.Echo, uc *user.UserController, ac *auth.AuthController, a *address.AddressController) {
-	e.Use(middleware.Logger())
+	e.Pre(middleware.RemoveTrailingSlash())
+	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+		Format: "method=${method}, uri=${uri}, status=${status}",
+	}))
 
-	e.POST("/login", ac.Login())
 	e.POST("/users", uc.Insert())
-	e.PUT("/users", uc.Update())
-	e.DELETE("/users", uc.Delete())
-	e.GET("/users", uc.Get())
-	
+	e.POST("/login", ac.Login())
+	e.GET("/users", uc.Get(), middlewares.JWTMiddleware())
+	e.PUT("/users", uc.Update(), middlewares.JWTMiddleware())
+	e.DELETE("/users", uc.Delete(), middlewares.JWTMiddleware())
+
 	eAddress := e.Group("")
-	eAddress.Use(middleware.JWT([]byte(configs.JWT_SECRET)))
+	eAddress.Use(middlewares.JWTMiddleware())
 	eAddress.POST("/addresses", a.Insert())
 	eAddress.GET("/addresses", a.Get())
 	eAddress.PUT("/addresses", a.Update())
 	eAddress.DELETE("/addresses", a.Delete())
-
 }
