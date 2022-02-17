a813bfbc948fdef0ca1d79bf49659ae5d3de1312
ynwahid <yusufnurwahid.contact@gmail.com>
2022-02-17 07:51:57 +0700

    change query

--- repository/auth/auth.go
+++ repository/auth/auth.go
@@ -19,8 +19,7 @@ func New(db *gorm.DB) *AuthRepository {
 // =========================== Login =================================
 func (a *AuthRepository) Login(email, password string) (u.Users, error) {
 	loggedInUser := u.Users{Email: email, Password: password}
-
-	if err := a.db.First(&loggedInUser).Error; err != nil {
+	if err := a.db.Model(&u.Users{}).Where("email = ? AND password = ?", email, password).First(&loggedInUser).Error; err != nil {
 		return loggedInUser, err
 	}
 	return loggedInUser, nil
