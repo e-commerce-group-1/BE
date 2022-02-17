3d558f252cb5878702ed54905ccebc436a69eb92
ynwahid <yusufnurwahid.contact@gmail.com>
2022-02-17 14:32:55 +0700

    change structure and rename variables

--- repository/order/interface.go
+++ repository/order/interface.go
@@ -3,8 +3,8 @@ package order
 import o "group-project1/entities/order"
 
 type Order interface {
+	Insert(NewOrder o.Orders) (o.Orders, error)
 	Get() ([]o.Orders, error)
-	Insert(newOrder o.Orders) (o.Orders, error)
-	Update(orderId int, newOrder o.Orders) (o.Orders, error)
-	Delete(orderId int) error
+	Update(UpdatedOrder o.Orders) (o.Orders, error)
+	Delete(ID int) error
 }
