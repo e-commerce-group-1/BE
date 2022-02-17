0a75b3e2fcd9f068528be2bd26e198236a5e5e97
ynwahid <yusufnurwahid.contact@gmail.com>
2022-02-17 16:31:20 +0700

    transaction detail test done

--- repository/transaction_detail/transaction_detail_test.go
+++ repository/transaction_detail/transaction_detail_test.go
@@ -18,7 +18,20 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"gorm.io/gorm"
 )
+
+func setup() *gorm.DB {
+	config := configs.GetConfig()
+	db := utils.InitDB(config)
+
+	db.Migrator().DropTable(user.Users{}, address.Addresses{}, transaction.Transactions{}, order.Orders{},
+		payment_method.PaymentMethods{}, product.Products{}, product_category.ProductCategories{},
+		transaction_detail.TransactionDetails{},
+	)
+	db.AutoMigrate(&user.Users{}, &transaction.Transactions{}, product_category.ProductCategories{}, product.Products{}, transaction_detail.TransactionDetails{})
+	return db
+}
 
 func TestInsert(t *testing.T) {
 	config := configs.GetConfig()
@@ -94,180 +107,209 @@ func TestInsert(t *testing.T) {
 	})
 }
 
-// func TestGet(t *testing.T) {
-// 	config := configs.GetConfig()
-// 	db := utils.InitDB(config)
+func TestGet(t *testing.T) {
+	db := setup()
 
-// 	db.Migrator().DropTable(user.Users{}, address.Addresses{}, transaction.Transactions{}, order.Orders{},
-// 		payment_method.PaymentMethods{}, product.Products{}, product_category.ProductCategories{},
-// 		transaction_detail.TransactionDetails{},
-// 	)
-// 	db.AutoMigrate(&order.Orders{}, &user.Users{}, &transaction.Transactions{})
+	repo := New(db)
+	userRepo := UserRepo.New(db)
+	trxRepo := TrxRepo.New(db)
+	productRepo := ProductRepo.New(db)
+	productCatRepo := ProductCatRepo.New(db)
 
-// 	repo := New(db)
-// 	userRepo := UserRepo.New(db)
-// 	trxRepo := TrxRepo.New(db)
-// 	pmRepo := PM.New(db)
+	t.Run("fail to get all transaction detail", func(t *testing.T) {
+		res, err := repo.Get()
+		assert.Nil(t, res)
+		assert.NotNil(t, err)
+	})
 
-// 	t.Run("fail to get all transactions", func(t *testing.T) {
-// 		res, err := repo.Get()
-// 		assert.Nil(t, res)
-// 		assert.NotNil(t, err)
-// 	})
+	t.Run("succeed to get all transaction detail", func(t *testing.T) {
+		mockUser := user.Users{
+			Name:     "Ucup",
+			UserName: "ucup",
+			Email:    "ucup@ucup.com",
+			Password: "ucup123",
+		}
+		userRepo.Insert(mockUser)
 
-// 	t.Run("succeed to get all transactions", func(t *testing.T) {
-// 		mockUser := user.Users{
-// 			Name:     "Ucup",
-// 			UserName: "ucup",
-// 			Email:    "ucup@ucup.com",
-// 			Password: "ucup123",
-// 		}
-// 		userRepo.Insert(mockUser)
+		mockT := transaction.Transactions{
+			TotalQty:   3,
+			TotalPrice: 4500000,
+			UserID:     1,
+		}
+		trxRepo.Insert(mockT)
 
-// 		mockT := transaction.Transactions{
-// 			TotalQty:   3,
-// 			TotalPrice: 4500000,
-// 			UserID:     1,
-// 		}
-// 		trxRepo.Insert(mockT)
+		mockPC := product_category.ProductCategories{
+			Name: "Sneakers",
+		}
+		mockP := product.Products{
+			Name:              "Sneaker 1",
+			Description:       "Produk 1",
+			Gender:            true,
+			Size:              40,
+			Price:             1000000,
+			Stock:             5,
+			ProductCategoryID: 1,
+		}
+		productCatRepo.Insert(mockPC)
+		productRepo.Insert(mockP)
 
-// 		mockPM := payment_method.PaymentMethods{
-// 			Name: "Gopay",
-// 		}
-// 		pmRepo.Insert(mockPM)
+		mockTD := transaction_detail.TransactionDetails{
+			Qty:           1,
+			Price:         1000000,
+			ProductID:     1,
+			TransactionID: 1,
+		}
 
-// 		mockO := order.Orders{
-// 			Phone:           "0812345678910",
-// 			TransactionID:   1,
-// 			PaymentMethodID: 1,
-// 		}
-// 		mockO2 := order.Orders{
-// 			Phone:           "0812345678911",
-// 			TransactionID:   1,
-// 			PaymentMethodID: 1,
-// 		}
-// 		repo.Insert(mockO)
-// 		repo.Insert(mockO2)
+		mockTD2 := transaction_detail.TransactionDetails{
+			Qty:           2,
+			Price:         2000000,
+			ProductID:     1,
+			TransactionID: 1,
+		}
 
-// 		res, err := repo.Get()
+		repo.Insert(mockTD)
+		repo.Insert(mockTD2)
 
-// 		assert.Nil(t, err)
-// 		assert.Equal(t, mockO.Phone, res[0].Phone)
-// 		assert.Equal(t, mockO2.Phone, res[1].Phone)
-// 	})
-// }
+		res, err := repo.Get()
+		assert.Nil(t, err)
+		assert.Equal(t, mockTD.Qty, res[0].Qty)
+		assert.Equal(t, mockTD.Price, res[0].Price)
+		assert.Equal(t, mockTD.ProductID, res[0].ProductID)
+		assert.Equal(t, mockTD.TransactionID, res[0].TransactionID)
+		assert.Equal(t, mockTD2.Qty, res[1].Qty)
+		assert.Equal(t, mockTD2.Price, res[1].Price)
+		assert.Equal(t, mockTD2.ProductID, res[1].ProductID)
+		assert.Equal(t, mockTD2.TransactionID, res[1].TransactionID)
+	})
+}
 
-// func TestUpdate(t *testing.T) {
-// 	config := configs.GetConfig()
-// 	db := utils.InitDB(config)
+func TestUpdate(t *testing.T) {
+	db := setup()
 
-// 	db.Migrator().DropTable(user.Users{}, address.Addresses{}, transaction.Transactions{}, order.Orders{},
-// 		payment_method.PaymentMethods{}, product.Products{}, product_category.ProductCategories{},
-// 		transaction_detail.TransactionDetails{},
-// 	)
-// 	db.AutoMigrate(&order.Orders{}, &user.Users{}, &transaction.Transactions{})
+	repo := New(db)
+	userRepo := UserRepo.New(db)
+	trxRepo := TrxRepo.New(db)
+	productRepo := ProductRepo.New(db)
+	productCatRepo := ProductCatRepo.New(db)
 
-// 	repo := New(db)
-// 	userRepo := UserRepo.New(db)
-// 	trxRepo := TrxRepo.New(db)
-// 	pmRepo := PM.New(db)
+	t.Run("succeed to update order", func(t *testing.T) {
+		mockUser := user.Users{
+			Name:     "Ucup",
+			UserName: "ucup",
+			Email:    "ucup@ucup.com",
+			Password: "ucup123",
+		}
+		userRepo.Insert(mockUser)
 
-// 	t.Run("succeed to update order", func(t *testing.T) {
-// 		mockUser := user.Users{
-// 			Name:     "Ucup",
-// 			UserName: "ucup",
-// 			Email:    "ucup@ucup.com",
-// 			Password: "ucup123",
-// 		}
-// 		userRepo.Insert(mockUser)
+		mockT := transaction.Transactions{
+			TotalQty:   3,
+			TotalPrice: 4500000,
+			UserID:     1,
+		}
+		trxRepo.Insert(mockT)
 
-// 		mockT := transaction.Transactions{
-// 			TotalQty:   3,
-// 			TotalPrice: 4500000,
-// 			UserID:     1,
-// 		}
-// 		trxRepo.Insert(mockT)
+		mockPC := product_category.ProductCategories{
+			Name: "Sneakers",
+		}
+		mockP := product.Products{
+			Name:              "Sneaker 1",
+			Description:       "Produk 1",
+			Gender:            true,
+			Size:              40,
+			Price:             1000000,
+			Stock:             5,
+			ProductCategoryID: 1,
+		}
+		productCatRepo.Insert(mockPC)
+		productRepo.Insert(mockP)
 
-// 		mockPM := payment_method.PaymentMethods{
-// 			Name: "Gopay",
-// 		}
-// 		pmRepo.Insert(mockPM)
+		mockTD := transaction_detail.TransactionDetails{
+			Qty:           1,
+			Price:         1000000,
+			ProductID:     1,
+			TransactionID: 1,
+		}
 
-// 		mockO := order.Orders{
-// 			Phone:           "0812345678910",
-// 			TransactionID:   1,
-// 			PaymentMethodID: 1,
-// 		}
-// 		mockO2 := order.Orders{
-// 			Model: gorm.Model{ID: 1},
-// 			Phone: "0812345678911",
-// 		}
-// 		repo.Insert(mockO)
+		mockTD2 := transaction_detail.TransactionDetails{
+			Model: gorm.Model{ID: 1},
+			Qty:   2,
+			Price: 2000000,
+		}
 
-// 		res, err := repo.Update(mockO2)
-// 		assert.Nil(t, err)
-// 		assert.Equal(t, mockO2.Phone, res.Phone)
-// 	})
+		repo.Insert(mockTD)
 
-// 	t.Run("fail to update order", func(t *testing.T) {
-// 		mockO2 := order.Orders{
-// 			Phone: "0812345678911",
-// 		}
+		res, err := repo.Update(mockTD2)
+		assert.Nil(t, err)
+		assert.Equal(t, mockTD2.Qty, res.Qty)
+		assert.Equal(t, mockTD2.Price, res.Price)
+	})
 
-// 		_, err := repo.Update(mockO2)
-// 		assert.NotNil(t, err)
-// 	})
-// }
+	t.Run("fail to update order", func(t *testing.T) {
+		mockTD2 := transaction_detail.TransactionDetails{
+			Model: gorm.Model{ID: 3},
+			Qty:   2,
+			Price: 2000000,
+		}
 
-// func TestDelete(t *testing.T) {
-// 	config := configs.GetConfig()
-// 	db := utils.InitDB(config)
+		_, err := repo.Update(mockTD2)
+		assert.NotNil(t, err)
+	})
+}
 
-// 	db.Migrator().DropTable(user.Users{}, address.Addresses{}, transaction.Transactions{}, order.Orders{},
-// 		payment_method.PaymentMethods{}, product.Products{}, product_category.ProductCategories{},
-// 		transaction_detail.TransactionDetails{},
-// 	)
-// 	db.AutoMigrate(&order.Orders{}, &user.Users{}, &transaction.Transactions{})
+func TestDelete(t *testing.T) {
+	db := setup()
 
-// 	repo := New(db)
-// 	userRepo := UserRepo.New(db)
-// 	trxRepo := TrxRepo.New(db)
-// 	pmRepo := PM.New(db)
+	repo := New(db)
+	userRepo := UserRepo.New(db)
+	trxRepo := TrxRepo.New(db)
+	productRepo := ProductRepo.New(db)
+	productCatRepo := ProductCatRepo.New(db)
 
-// 	t.Run("fail to delete order", func(t *testing.T) {
-// 		err := repo.Delete(1)
-// 		assert.NotNil(t, err)
-// 	})
+	t.Run("fail to delete order", func(t *testing.T) {
+		err := repo.Delete(1)
+		assert.NotNil(t, err)
+	})
 
-// 	t.Run("succeed to delete order", func(t *testing.T) {
-// 		mockUser := user.Users{
-// 			Name:     "Ucup",
-// 			UserName: "ucup",
-// 			Email:    "ucup@ucup.com",
-// 			Password: "ucup123",
-// 		}
-// 		userRepo.Insert(mockUser)
+	t.Run("succeed to delete order", func(t *testing.T) {
+		mockUser := user.Users{
+			Name:     "Ucup",
+			UserName: "ucup",
+			Email:    "ucup@ucup.com",
+			Password: "ucup123",
+		}
+		userRepo.Insert(mockUser)
 
-// 		mockT := transaction.Transactions{
-// 			TotalQty:   3,
-// 			TotalPrice: 4500000,
-// 			UserID:     1,
-// 		}
-// 		trxRepo.Insert(mockT)
+		mockT := transaction.Transactions{
+			TotalQty:   3,
+			TotalPrice: 4500000,
+			UserID:     1,
+		}
+		trxRepo.Insert(mockT)
 
-// 		mockPM := payment_method.PaymentMethods{
-// 			Name: "Gopay",
-// 		}
-// 		pmRepo.Insert(mockPM)
+		mockPC := product_category.ProductCategories{
+			Name: "Sneakers",
+		}
+		mockP := product.Products{
+			Name:              "Sneaker 1",
+			Description:       "Produk 1",
+			Gender:            true,
+			Size:              40,
+			Price:             1000000,
+			Stock:             5,
+			ProductCategoryID: 1,
+		}
+		productCatRepo.Insert(mockPC)
+		productRepo.Insert(mockP)
 
-// 		mockO := order.Orders{
-// 			Phone:           "0812345678910",
-// 			TransactionID:   1,
-// 			PaymentMethodID: 1,
-// 		}
-// 		repo.Insert(mockO)
+		mockTD := transaction_detail.TransactionDetails{
+			Qty:           1,
+			Price:         1000000,
+			ProductID:     1,
+			TransactionID: 1,
+		}
+		repo.Insert(mockTD)
 
-// 		err := repo.Delete(1)
-// 		assert.Nil(t, err)
-// 	})
-// }
+		err := repo.Delete(1)
+		assert.Nil(t, err)
+	})
+}
