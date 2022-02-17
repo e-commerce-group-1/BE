cd840d36a4aabc32b8e0ec11f517be7e3a06d547
ynwahid <yusufnurwahid.contact@gmail.com>
2022-02-17 21:52:30 +0700

    auth controller done

--- deliveries/controllers/auth/auth.go
+++ deliveries/controllers/auth/auth.go
@@ -1,13 +1,11 @@
 package auth
 
 import (
-	"group-project1/configs"
-	"group-project1/entities/user"
+	"group-project1/deliveries/controllers/common"
+	"group-project1/deliveries/middlewares"
 	"group-project1/repository/auth"
 	"net/http"
-	"time"
 
-	"github.com/golang-jwt/jwt"
 	"github.com/labstack/echo/v4"
 )
 
@@ -15,59 +13,28 @@ type AuthController struct {
 	repo auth.Auth
 }
 
-func New(repo auth.Auth) *AuthController {
+func New(repository auth.Auth) *AuthController {
 	return &AuthController{
-		repo: repo,
+		repo: repository,
 	}
 }
 
-func (a AuthController) Login() echo.HandlerFunc {
+func (a *AuthController) Login() echo.HandlerFunc {
 	return func(c echo.Context) error {
 		loginFormat := LoginRequestFormat{}
-
-		if err := c.Bind(&loginFormat); err != nil {
-			return c.JSON(http.StatusBadRequest, "Ada yang salah dengan input")
+		if err := c.Bind(&loginFormat); err != nil || loginFormat.Email == "" || loginFormat.Password == "" {
+			return c.JSON(http.StatusBadRequest, common.BadRequest())
 		}
 
 		checkedUser, err := a.repo.Login(loginFormat.Email, loginFormat.Password)
-
 		if err != nil {
-			return c.JSON(http.StatusInternalServerError, "Email atau password tidak valid")
+			return c.JSON(http.StatusInternalServerError, common.InternalServerError())
 		}
 
-		token, err := GenerateToken(checkedUser)
-
+		tokenID, err := middlewares.GenerateToken(checkedUser.ID)
 		if err != nil {
-			return c.JSON(http.StatusNotAcceptable, "Terjadi error ketika melakukan generate token")
+			return c.JSON(http.StatusNotAcceptable, common.NotAcceptable())
 		}
-
-		return c.JSON(http.StatusOK, LoginResponseFormat{
-			Code: 200,
-			Success: true,
-			Message: "Success Login",
-			Data: checkedUser,
-			Token: token,
-		})
+		return c.JSON(http.StatusOK, common.Success(http.StatusOK, "successfully logged in, got new token", tokenID))
 	}
-}
-
-func GenerateToken(user user.Users) (string, error) {
-	datas := jwt.MapClaims{}
-	datas["id"] = user.ID
-	datas["exp"] = time.Now().Add(time.Hour * 1).Unix() //1jam
-	datas["authorized"] = true
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, datas)
-	return token.SignedString([]byte(configs.JWT_SECRET))
-}
-
-func ExtractTokenUserId(e echo.Context) float64 {
-
-	user := e.Get("user").(*jwt.Token)
-	if user.Valid {
-		datas := user.Claims.(jwt.MapClaims)
-		userId := datas["id"].(float64)
-		return userId
-	}
-
-	return 0
 }
--- deliveries/controllers/auth/formatter.go
+++ deliveries/controllers/auth/formatter.go
@@ -1,16 +1,6 @@
 package auth
 
-import "group-project1/entities/user"
-
 type LoginRequestFormat struct {
-	Email string `json:"email" form:"email"`
-	Password   string `json:"password" form:"password"`
-}
-
-type LoginResponseFormat struct {
-	Code int `json:"code"`
-	Success bool `json:"success"`
-	Message string        `json:"message"`
-	Data    user.Users `json:"data"`
-	Token   string        `json:"token"`
+	Email    string `json:"email" form:"email"`
+	Password string `json:"password" form:"password"`
 }
