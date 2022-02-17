52edc14bc4dee681195f33ca964d6c2f0a77d8b4
ynwahid <yusufnurwahid.contact@gmail.com>
2022-02-17 11:18:15 +0700

    change structures and conditions

--- repository/product_category/product_category.go
+++ repository/product_category/product_category.go
@@ -1,9 +1,9 @@
 package product_category
 
 import (
+	"errors"
 	pc "group-project1/entities/product_category"
 
-	"github.com/labstack/gommon/log"
 	"gorm.io/gorm"
 )
 
@@ -16,49 +16,41 @@ func New(db *gorm.DB) *ProductCategoryRepository {
 }
 
 // ======================== Insert Product Category ================================
-func (ur *ProductCategoryRepository) Insert(newProductCategory pc.ProductCategories) (pc.ProductCategories, error) {
-	if err := ur.db.Save(&newProductCategory).Error; err != nil {
-		log.Warn("Found database error:", err)
-		return newProductCategory, err
+func (ur *ProductCategoryRepository) Insert(NewProductCategory pc.ProductCategories) (pc.ProductCategories, error) {
+	if err := ur.db.Create(&NewProductCategory).Error; err != nil {
+		return NewProductCategory, err
 	}
-
-	return newProductCategory, nil
+	return NewProductCategory, nil
 }
 
 // ======================== Get Product Categories ==================================
 func (ur *ProductCategoryRepository) Get() ([]pc.ProductCategories, error) {
 	product_categories := []pc.ProductCategories{}
-	if err := ur.db.Find(&product_categories).Error; err != nil {
-		log.Warn("Found database error:", err)
-		return nil, err
+	ur.db.Find(&product_categories)
+	if len(product_categories) == 0 {
+		return nil, errors.New("belum ada kategori produk yang terdaftar")
 	}
 	return product_categories, nil
 }
 
 // ======================== Update Product Category ===============================
-func (ur *ProductCategoryRepository) Update(product_categoryId int, newProductCategory pc.ProductCategories) (pc.ProductCategories, error) {
-
-	var product_category pc.ProductCategories
-	ur.db.First(&product_category, product_categoryId)
-
-	if err := ur.db.Model(&product_category).Updates(&newProductCategory).Error; err != nil {
-		return product_category, err
+func (ur *ProductCategoryRepository) Update(NewProductCategory pc.ProductCategories) (pc.ProductCategories, error) {
+	res := ur.db.Model(&NewProductCategory).Updates(NewProductCategory)
+	if res.RowsAffected == 0 {
+		return NewProductCategory, errors.New("tidak ada pemutakhiran pada kategori produk")
 	}
-
-	return product_category, nil
+	ur.db.First(&NewProductCategory)
+	return NewProductCategory, nil
 }
 
 // ======================== Delete Product Category ===============================
-func (ur *ProductCategoryRepository) Delete(product_categoryId int) error {
-
+func (ur *ProductCategoryRepository) Delete(ID int) error {
 	var product_category pc.ProductCategories
-
-	if err := ur.db.First(&product_category, product_categoryId).Error; err != nil {
-		return err
+	res := ur.db.Delete(&product_category, ID)
+	if res.RowsAffected == 0 {
+		return errors.New("tidak ada kategori produk yang dihapus")
 	}
-	ur.db.Delete(&product_category, product_categoryId)
 	return nil
-
 }
 
 // ============================================================================
