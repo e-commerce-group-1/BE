c1748b3ae0a826b393da83e5fa03eb815ac5eb9e
ynwahid <yusufnurwahid.contact@gmail.com>
2022-02-19 00:58:56 +0700

    product controller done

--- deliveries/controllers/product/formatter.go
+++ deliveries/controllers/product/formatter.go
@@ -4,52 +4,117 @@ import "group-project1/entities/product"
 
 // =================== Create Product ======================
 type CreateProductRequestFormat struct {
-	Name string `json:"name" form:"name"`
+	Name        string `json:"name" form:"name"`
 	Description string `json:"description" form:"description"`
-	Gender bool `json:"gender" form:"gender"`
-	Size int `json:"size" form:"size"`  
-	Price int `json:"price" form:"price"`
-	Stock int `json:"stock" form:"stock"`  
-	Image string `json:"image" form:"image"`
+	Gender      bool   `json:"gender" form:"gender"`
+	Size        uint   `json:"size" form:"size"`
+	Price       uint   `json:"price" form:"price"`
+	Stock       uint   `json:"stock" form:"stock"`
+	Image       string `json:"image" form:"image"`
+}
+
+func (CPRF CreateProductRequestFormat) ToCreateProductEntity(ProductCategoryID uint) product.Products {
+	return product.Products{
+		Name:              CPRF.Name,
+		Description:       CPRF.Description,
+		Gender:            CPRF.Gender,
+		Size:              CPRF.Size,
+		Price:             CPRF.Price,
+		Stock:             CPRF.Stock,
+		Image:             CPRF.Image,
+		ProductCategoryID: ProductCategoryID,
+	}
 }
 
 type CreateProductResponseFormat struct {
-	Code int `json:"code"`
-	Success bool `json:"success"`
-	Message string        `json:"message"`
-	Data    product.Products `json:"data"`
+	Name        string `json:"name" form:"name"`
+	Description string `json:"description" form:"description"`
+	Gender      bool   `json:"gender" form:"gender"`
+	Size        uint   `json:"size" form:"size"`
+	Price       uint   `json:"price" form:"price"`
+	Stock       uint   `json:"stock" form:"stock"`
+	Image       string `json:"image" form:"image"`
 }
 
-// =================== Get Products =======================
-type GetProductsResponseFormat struct {
-	Code int `json:"code"`
-	Success bool `json:"success"`
-	Message string        `json:"message"`
-	Data   []product.Products `json:"data"`
+func ToCreateProductResponseFormat(P product.Products) CreateProductResponseFormat {
+	return CreateProductResponseFormat{
+		Name:        P.Name,
+		Description: P.Description,
+		Gender:      P.Gender,
+		Size:        P.Size,
+		Price:       P.Price,
+		Stock:       P.Stock,
+		Image:       P.Image,
+	}
 }
 
 // =================== Update Product ======================
 type UpdateProductRequestFormat struct {
-	Name string `json:"name" form:"name"`
+	Name        string `json:"name" form:"name"`
 	Description string `json:"description" form:"description"`
-	Gender bool `json:"gender" form:"gender"`
-	Size int `json:"size" form:"size"`  
-	Price int `json:"price" form:"price"`
-	Stock int `json:"stock" form:"stock"`  
-	Image string `json:"image" form:"image"`
+	Gender      bool   `json:"gender" form:"gender"`
+	Size        uint   `json:"size" form:"size"`
+	Price       uint   `json:"price" form:"price"`
+	Stock       uint   `json:"stock" form:"stock"`
+	Image       string `json:"image" form:"image"`
+}
+
+func (UPRF UpdateProductRequestFormat) ToUpdateProductEntity(ProductCategoryID uint) product.Products {
+	return product.Products{
+		Name:              UPRF.Name,
+		Description:       UPRF.Description,
+		Gender:            UPRF.Gender,
+		Size:              UPRF.Size,
+		Price:             UPRF.Price,
+		Stock:             UPRF.Stock,
+		Image:             UPRF.Image,
+		ProductCategoryID: ProductCategoryID,
+	}
 }
 
 type UpdateProductResponseFormat struct {
-	Code int `json:"code"`
-	Success bool `json:"success"`
-	Message string        `json:"message"`
-	Data    product.Products `json:"data"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	Gender      bool   `json:"gender"`
+	Size        uint   `json:"size"`
+	Price       uint   `json:"price"`
+	Stock       uint   `json:"stock"`
+	Image       string `json:"image"`
 }
 
-// =================== Delete Product ======================
-type DeleteProductResponseFormat struct {
-	Code int `json:"code"`
-	Success bool `json:"success"`
-	Message string        `json:"message"`
-	Data   product.Products `json:"data"`
+func ToUpdateProductResponseFormat(P product.Products) UpdateProductResponseFormat {
+	return UpdateProductResponseFormat{
+		Name:        P.Name,
+		Description: P.Description,
+		Gender:      P.Gender,
+		Size:        P.Size,
+		Price:       P.Price,
+		Stock:       P.Stock,
+		Image:       P.Image,
+	}
+}
+
+// =================== Get Products =======================
+type GetProductResponseFormat struct {
+	Name        string `json:"name" form:"name"`
+	Description string `json:"description" form:"description"`
+	Gender      bool   `json:"gender" form:"gender"`
+	Size        uint   `json:"size" form:"size"`
+	Price       uint   `json:"price" form:"price"`
+	Stock       uint   `json:"stock" form:"stock"`
+	Image       string `json:"image" form:"image"`
+}
+
+func ToProductGetResponseFormat(Responses []product.Products) []GetProductResponseFormat {
+	GetResponses := make([]GetProductResponseFormat, len(Responses))
+	for i := 0; i < len(Responses); i++ {
+		GetResponses[i].Name = Responses[i].Name
+		GetResponses[i].Description = Responses[i].Description
+		GetResponses[i].Gender = Responses[i].Gender
+		GetResponses[i].Size = Responses[i].Size
+		GetResponses[i].Price = Responses[i].Price
+		GetResponses[i].Stock = Responses[i].Stock
+		GetResponses[i].Image = Responses[i].Image
+	}
+	return GetResponses
 }
--- deliveries/controllers/product/product.go
+++ deliveries/controllers/product/product.go
@@ -1,1 +1,84 @@
 package product
+
+import (
+	"group-project1/deliveries/controllers/common"
+	"group-project1/deliveries/middlewares"
+	"group-project1/repository/product"
+	"net/http"
+	"strconv"
+
+	"github.com/labstack/echo/v4"
+)
+
+type ProductController struct {
+	repo product.ProductRepository
+}
+
+func New(repository product.ProductRepository) *ProductController {
+	return &ProductController{
+		repo: repository,
+	}
+}
+
+func (uc *ProductController) Insert() echo.HandlerFunc {
+	return func(c echo.Context) error {
+		NewProduct := CreateProductRequestFormat{}
+		ProductCategoryID, _ := strconv.Atoi(c.Param("id"))
+		if err := c.Bind(&NewProduct); err != nil || NewProduct.Name == "" {
+			return c.JSON(http.StatusBadRequest, common.BadRequest())
+		}
+		res, err := uc.repo.Insert(NewProduct.ToCreateProductEntity(uint(ProductCategoryID)))
+
+		if err != nil {
+			return c.JSON(http.StatusInternalServerError, common.InternalServerError())
+		}
+		return c.JSON(http.StatusCreated, common.Success(http.StatusCreated, "sukses menambahkan produk baru", ToCreateProductResponseFormat(res)))
+	}
+}
+
+func (uc *ProductController) Get() echo.HandlerFunc {
+	return func(c echo.Context) error {
+		res, err := uc.repo.Get()
+		if err != nil {
+			return c.JSON(http.StatusInternalServerError, common.InternalServerError())
+		}
+		return c.JSON(http.StatusOK, common.Success(http.StatusOK, "sukses mendapatkan semua produk", ToProductGetResponseFormat(res)))
+	}
+}
+
+func (uc *ProductController) Update() echo.HandlerFunc {
+	return func(c echo.Context) error {
+		isAdmin := middlewares.ExtractTokenIsAdmin(c)
+		if !isAdmin {
+			return c.JSON(http.StatusUnauthorized, common.UnAuthorized())
+		}
+		ProductCategoryID, _ := strconv.Atoi(c.Param("id"))
+		var UpdatedProduct = UpdateProductRequestFormat{}
+
+		if err := c.Bind(&UpdatedProduct); err != nil {
+			return c.JSON(http.StatusBadRequest, common.BadRequest())
+		}
+
+		res, err := uc.repo.Update(UpdatedProduct.ToUpdateProductEntity(uint(ProductCategoryID)))
+
+		if err != nil {
+			return c.JSON(http.StatusInternalServerError, common.InternalServerError())
+		}
+		return c.JSON(http.StatusOK, common.Success(http.StatusOK, "sukses update produk", ToUpdateProductResponseFormat(res)))
+	}
+}
+
+func (uc *ProductController) Delete() echo.HandlerFunc {
+	return func(c echo.Context) error {
+		isAdmin := middlewares.ExtractTokenIsAdmin(c)
+		if !isAdmin {
+			return c.JSON(http.StatusUnauthorized, common.UnAuthorized())
+		}
+		ProductCategoryID, _ := strconv.Atoi(c.Param("id"))
+		err := uc.repo.Delete(ProductCategoryID)
+		if err != nil {
+			return c.JSON(http.StatusInternalServerError, common.InternalServerError())
+		}
+		return c.JSON(http.StatusOK, common.Success(http.StatusOK, "sukses menghapus produk", err))
+	}
+}
