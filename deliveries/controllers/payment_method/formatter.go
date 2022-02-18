6332405ec24402e23e2e641af07840b88eb593d9
ynwahid <yusufnurwahid.contact@gmail.com>
2022-02-18 23:26:24 +0700

    payment method controller

--- deliveries/controllers/payment_method/formatter.go
+++ deliveries/controllers/payment_method/formatter.go
@@ -1,1 +1,99 @@
 package payment_method
+
+import (
+	"group-project1/deliveries/controllers/common"
+	"group-project1/deliveries/middlewares"
+	PMEntity "group-project1/entities/payment_method"
+	PM "group-project1/repository/payment_method"
+	"net/http"
+	"strconv"
+
+	"github.com/labstack/echo/v4"
+)
+
+type PMController struct {
+	repo PM.PaymentMethodRepository
+}
+
+func New(repository PM.PaymentMethodRepository) *PMController {
+	return &PMController{
+		repo: repository,
+	}
+}
+
+func (uc *PMController) Insert() echo.HandlerFunc {
+	return func(c echo.Context) error {
+		isAdmin := middlewares.ExtractTokenIsAdmin(c)
+		if !isAdmin {
+			return c.JSON(http.StatusUnauthorized, common.UnAuthorized())
+		}
+
+		NewPM := CreatePMRequestFormat{}
+
+		if err := c.Bind(&NewPM); err != nil || NewPM.Name == "" {
+			return c.JSON(http.StatusBadRequest, common.BadRequest())
+		}
+
+		newPM := PMEntity.PaymentMethods{
+			Name: NewPM.Name,
+		}
+		res, err := uc.repo.Insert(newPM)
+
+		if err != nil {
+			return c.JSON(http.StatusInternalServerError, common.InternalServerError())
+		}
+		return c.JSON(http.StatusCreated, common.Success(http.StatusCreated, "sukses menambahkan kategori payment method baru", ToCreatePMResponseFormat(res)))
+	}
+}
+
+func (uc *PMController) Update() echo.HandlerFunc {
+	return func(c echo.Context) error {
+		isAdmin := middlewares.ExtractTokenIsAdmin(c)
+		if !isAdmin {
+			return c.JSON(http.StatusUnauthorized, common.UnAuthorized())
+		}
+		PMID, _ := strconv.Atoi((c.Param("id")))
+		var UpdatedPM = UpdatePMRequestFormat{}
+
+		if err := c.Bind(&UpdatedPM); err != nil {
+			return c.JSON(http.StatusBadRequest, common.BadRequest())
+		}
+
+		res, err := uc.repo.Update(UpdatedPM.ToUpdatePMRequestFormat(uint(PMID)))
+
+		if err != nil {
+			return c.JSON(http.StatusInternalServerError, common.InternalServerError())
+		}
+		return c.JSON(http.StatusOK, common.Success(http.StatusOK, "sukses update payment method baru", ToUpdatePMResponseFormat(res)))
+	}
+}
+
+func (uc *PMController) Get() echo.HandlerFunc {
+	return func(c echo.Context) error {
+		isAdmin := middlewares.ExtractTokenIsAdmin(c)
+		if !isAdmin {
+			return c.JSON(http.StatusUnauthorized, common.UnAuthorized())
+		}
+
+		res, err := uc.repo.Get()
+		if err != nil {
+			return c.JSON(http.StatusInternalServerError, common.InternalServerError())
+		}
+		return c.JSON(http.StatusOK, common.Success(http.StatusOK, "sukses mendapatkan semua payment method", ToPMGetResponseFormat(res)))
+	}
+}
+
+func (uc *PMController) Delete() echo.HandlerFunc {
+	return func(c echo.Context) error {
+		isAdmin := middlewares.ExtractTokenIsAdmin(c)
+		if !isAdmin {
+			return c.JSON(http.StatusUnauthorized, common.UnAuthorized())
+		}
+		PMID, _ := strconv.Atoi((c.Param("id")))
+		err := uc.repo.Delete(PMID)
+		if err != nil {
+			return c.JSON(http.StatusInternalServerError, common.InternalServerError())
+		}
+		return c.JSON(http.StatusOK, common.Success(http.StatusOK, "sukses menghapus payment method", err))
+	}
+}
--- deliveries/controllers/payment_method/payment_method.go
+++ deliveries/controllers/payment_method/payment_method.go
@@ -1,1 +1,56 @@
 package payment_method
+
+import (
+	PM "group-project1/entities/payment_method"
+
+	"gorm.io/gorm"
+)
+
+// =================== Create Payment Method =======================
+type CreatePMRequestFormat struct {
+	Name string `json:"name" form:"name"`
+}
+
+type CreatePMResponseFormat struct {
+	Name string `json:"name"`
+}
+
+func ToCreatePMResponseFormat(PMResponse PM.PaymentMethods) CreatePMResponseFormat {
+	return CreatePMResponseFormat{
+		Name: PMResponse.Name,
+	}
+}
+
+// =================== Update PM =======================
+type UpdatePMRequestFormat struct {
+	Name string `json:"name" form:"name"`
+}
+
+func (UURF UpdatePMRequestFormat) ToUpdatePMRequestFormat(ID uint) PM.PaymentMethods {
+	return PM.PaymentMethods{
+		Model: gorm.Model{ID: ID},
+		Name:  UURF.Name,
+	}
+}
+
+type UpdatePMResponseFormat struct {
+	Name string `json:"name"`
+}
+
+func ToUpdatePMResponseFormat(PMResponse PM.PaymentMethods) UpdatePMResponseFormat {
+	return UpdatePMResponseFormat{
+		Name: PMResponse.Name,
+	}
+}
+
+type PMGetResponseFormat struct {
+	Name string `json:"name"`
+}
+
+func ToPMGetResponseFormat(PMResponses []PM.PaymentMethods) []PMGetResponseFormat {
+	PMGetResponses := make([]PMGetResponseFormat, len(PMResponses))
+	for i := 0; i < len(PMResponses); i++ {
+		PMGetResponses[i].Name = PMResponses[i].Name
+	}
+	return PMGetResponses
+}
