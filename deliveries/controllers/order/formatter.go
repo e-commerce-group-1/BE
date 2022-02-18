aea638ad689fbc268e7238d106722a8206a09660
ynwahid <yusufnurwahid.contact@gmail.com>
2022-02-18 13:38:36 +0700

    fix formatter

--- deliveries/controllers/order/formatter.go
+++ deliveries/controllers/order/formatter.go
@@ -1,23 +1,24 @@
 package order
-import(
+
+import (
 	"time"
 )
+
 // =================== Create Order =======================
 type CreateOrderRequestFormat struct {
-	Phone string `json:"phone" form:"phone"`
-	Status time.Time `json:"status" form:"status"`
+	Phone  string `json:"phone" form:"phone"`
+	Status bool   `json:"status" form:"status"`
 }
-
 
 // =================== Update Order =======================
 type UpdateOrderRequestFormat struct {
-	Phone string `json:"phone" form:"phone"`
+	Phone  string    `json:"phone" form:"phone"`
 	Status time.Time `json:"status" form:"status"`
 }
 
 type OrderResponseFormat struct {
-	Code int `json:"code"`
-	Success bool `json:"success"`
-	Message string        `json:"message"`
+	Code    int         `json:"code"`
+	Success bool        `json:"success"`
+	Message string      `json:"message"`
 	Data    interface{} `json:"data"`
 }
