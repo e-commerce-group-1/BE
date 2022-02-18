df7a224c107f8b43e1632e4f8b16bfcd7974906e
ynwahid <yusufnurwahid.contact@gmail.com>
2022-02-18 21:42:49 +0700

    admin cotroller

---
+++ deliveries/controllers/admin/admin.go
@@ -0,0 +1,43 @@
+package admin
+
+import (
+	"group-project1/deliveries/controllers/common"
+	UserEntity "group-project1/entities/user"
+	"group-project1/repository/admin"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+type AdminController struct {
+	repo admin.Admin
+}
+
+func New(repository admin.Admin) *AdminController {
+	return &AdminController{
+		repo: repository,
+	}
+}
+
+func (ac *AdminController) Insert() echo.HandlerFunc {
+	return func(c echo.Context) error {
+		NewUser := CreateAdminRequestFormat{}
+
+		if err := c.Bind(&NewUser); err != nil || NewUser.Name == "" || NewUser.Email == "" || NewUser.Password == "" {
+			return c.JSON(http.StatusBadRequest, common.BadRequest())
+		}
+
+		newUser := UserEntity.Users{
+			Name:     NewUser.Name,
+			UserName: NewUser.UserName,
+			Email:    NewUser.Email,
+			Password: NewUser.Password,
+		}
+		res, err := ac.repo.Insert(newUser)
+
+		if err != nil {
+			return c.JSON(http.StatusInternalServerError, common.InternalServerError())
+		}
+		return c.JSON(http.StatusCreated, common.Success(http.StatusCreated, "sukses menambahkan admin baru", ToCreateAdminResponseFormat(res)))
+	}
+}
