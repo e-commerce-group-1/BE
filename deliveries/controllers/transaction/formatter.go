2608c5b57de9254620f4938fe3dec825bcf9bcc8
ynwahid <yusufnurwahid.contact@gmail.com>
2022-02-18 23:28:03 +0700

    reformat

--- deliveries/controllers/transaction/formatter.go
+++ deliveries/controllers/transaction/formatter.go
@@ -2,18 +2,19 @@ package transaction
 
 // =================== Create Transaction =======================
 type CreateTransactionRequestFormat struct {
-	TotalQty int `json:"total_qty" form:"total_qty"`
+	TotalQty   int `json:"total_qty" form:"total_qty"`
 	TotalPrice int `json:"total_price" form:"total_price"`
 }
+
 // =================== Update Address =======================
 type UpdateTransactionRequestFormat struct {
-	TotalQty int `json:"total_qty" form:"total_qty"`
+	TotalQty   int `json:"total_qty" form:"total_qty"`
 	TotalPrice int `json:"total_price" form:"total_price"`
 }
 
 type TransactionResponseFormat struct {
-	Code int `json:"code"`
-	Success bool `json:"success"`
-	Message string        `json:"message"`
+	Code    int         `json:"code"`
+	Success bool        `json:"success"`
+	Message string      `json:"message"`
 	Data    interface{} `json:"data"`
 }
