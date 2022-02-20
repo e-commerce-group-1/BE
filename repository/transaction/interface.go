ea0fa81d81033e3ca6ec37fc0c334145d64486b4
ynwahid <yusufnurwahid.contact@gmail.com>
2022-02-20 17:27:12 +0700

    transaction repo done

--- repository/transaction/interface.go
+++ repository/transaction/interface.go
@@ -1,12 +1,10 @@
 package transaction
 
-import tr "group-project1/entities/transaction"
+import t "group-project1/entities/transaction"
 
 type Transaction interface {
-	Insert(NewTrx tr.Transactions) (tr.Transactions, error)
-	GetByID(ProductID uint, UserID uint) (tr.Transactions, error)
-	UpdateByID(ProductID uint, UserID uint, UpdatedTrx tr.Transactions) (tr.Transactions, error)
+	Insert(NewTransaction t.Transactions) (t.Transactions, error)
+	GetAllTrxByUserID(UserID uint) ([]t.Transactions, error)
 	DeleteByID(ProductID uint, UserID uint) error
-	Get(UserID uint) ([]tr.Transactions, error)
 	FindID(UserID uint, ProductID uint) (uint, error)
 }
