8e15cd6b38bf53cb8add88a42c5df41e3ecccdf0
ynwahid <yusufnurwahid.contact@gmail.com>
2022-02-20 20:35:23 +0700

    remove address relationship

--- entities/user/user.go
+++ entities/user/user.go
@@ -1,7 +1,6 @@
 package user
 
 import (
-	"group-project1/entities/address"
 	"group-project1/entities/transaction"
 
 	"gorm.io/gorm"
@@ -14,6 +13,5 @@ type Users struct {
 	Email       string                     `gorm:"type:varchar(255); unique; not null"`
 	Password    string                     `gorm:"type:varchar(255); not null"`
 	IsAdmin     bool                       `gorm:"type: boolean"`
-	Address     address.Addresses          `gorm:"foreignKey:UserID"`
 	Transaction []transaction.Transactions `gorm:"foreignKey:UserID"`
 }
