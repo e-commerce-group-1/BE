4f91a652215a678951fa3cddd5e44984ba8261db
ynwahid <yusufnurwahid.contact@gmail.com>
2022-02-20 20:36:38 +0700

    transaction controller done

--- deliveries/controllers/transaction/transaction.go
+++ deliveries/controllers/transaction/transaction.go
@@ -1,56 +1,73 @@
 package transaction
 
-// import (
-// 	// "group-project1/deliveries/controllers/common"
-// 	// "group-project1/deliveries/middlewares"
-// 	"group-project1/repository/product"
-// 	transactionRepo "group-project1/repository/transaction"
-// 	// "net/http"
+import (
+	"group-project1/deliveries/controllers/common"
+	"group-project1/deliveries/middlewares"
+	TrxRepo "group-project1/repository/transaction"
+	"net/http"
+	"strconv"
 
-// 	// "github.com/labstack/echo/v4"
-// )
+	"github.com/labstack/echo/v4"
+)
 
-// type TransactionController struct {
-// 	Repo     transactionRepo.Transaction
-// 	RepoProd product.Product
-// }
+type TransactionController struct {
+	Repo TrxRepo.Transaction
+}
 
-// func New(repo transactionRepo.Transaction, repoProd product.Product) *TransactionController {
-// 	return &TransactionController{
-// 		Repo:     repo,
-// 		RepoProd: repoProd,
-// 	}
-// }
+func New(repo TrxRepo.Transaction) *TransactionController {
+	return &TransactionController{
+		Repo: repo,
+	}
+}
 
-// func (tc *TransactionController) Insert() echo.HandlerFunc {
-// 	return func(c echo.Context) error {
-// 		UserID := uint(middlewares.ExtractTokenUserId(c))
+func (tc *TransactionController) Insert() echo.HandlerFunc {
+	return func(c echo.Context) error {
+		UserID := middlewares.ExtractTokenUserId(c)
+		NewTrx := CreateTransactionRequestFormat{}
+		if err := c.Bind(&NewTrx); err != nil || NewTrx.Qty < 1 || NewTrx.Size == "" || NewTrx.ProductID < 1 {
+			return c.JSON(http.StatusBadRequest, common.BadRequest())
+		}
 
-// 		newTrx := CreateTransactionRequestFormat{}
-// 		if err := c.Bind(&newTrx); err != nil {
-// 			return c.JSON(http.StatusBadRequest, common.BadRequest())
-// 		}
+		res, err := tc.Repo.Insert(NewTrx.ToTransactionEntity(uint(UserID)))
+		if err != nil {
+			return c.JSON(http.StatusInternalServerError, common.InternalServerError())
+		}
+		return c.JSON(http.StatusCreated, common.Success(http.StatusCreated, "sukses menambahkan transaksi baru", ToTransactionResponseFormat(res)))
+	}
+}
 
-// 		_, err := tc.Repo.FindID(UserID, newTrx.ProductID)
+func (tc *TransactionController) GetAllTrxByUserID() echo.HandlerFunc {
+	return func(c echo.Context) error {
+		UserID := middlewares.ExtractTokenUserId(c)
 
-// 		if err != nil {
+		res, err := tc.Repo.GetAllTrxByUserID(uint(UserID))
+		if err != nil {
+			return c.JSON(http.StatusInternalServerError, common.InternalServerError())
+		}
+		return c.JSON(http.StatusOK, common.Success(http.StatusOK, "sukses mendapatkan semua transaksi berdasarkan UserID", ToTransactionResponseFormatArr(res)))
+	}
+}
 
-// 		}
-// 	}
-// }
-// func (tc *TransactionController) Get() echo.HandlerFunc {
-// 	return func(c echo.Context) error {
-// 		res, err := tc.Repo.Get()
+func (tc *TransactionController) DeleteByID() echo.HandlerFunc {
+	return func(c echo.Context) error {
+		UserID := middlewares.ExtractTokenUserId(c)
+		ProductID, _ := strconv.Atoi(c.Param("id"))
+		err := tc.Repo.DeleteByID(uint(ProductID), uint(UserID))
+		if err != nil {
+			return c.JSON(http.StatusInternalServerError, common.InternalServerError())
+		}
+		return c.JSON(http.StatusOK, common.Success(http.StatusOK, "sukses mendapatkan semua transaksi berdasarkan UserID", err))
+	}
+}
 
-// 		if err != nil {
-// 			return c.JSON(http.StatusInternalServerError, "Gagal Menampilkan Data Transaction")
-// 		}
-
-// 		return c.JSON(http.StatusOK, TransactionResponseFormat{
-// 			Code:    200,
-// 			Success: true,
-// 			Message: "Success Get Transaction",
-// 			Data:    res,
-// 		})
-// 	}
-// }
+func (tc *TransactionController) FindID() echo.HandlerFunc {
+	return func(c echo.Context) error {
+		UserID := middlewares.ExtractTokenUserId(c)
+		ProductID, _ := strconv.Atoi(c.Param("id"))
+		res, err := tc.Repo.FindID(uint(ProductID), uint(UserID))
+		if err != nil {
+			return c.JSON(http.StatusInternalServerError, common.InternalServerError())
+		}
+		return c.JSON(http.StatusOK, common.Success(http.StatusOK, "sukses mendapatkan ID transaksi", res))
+	}
+}
