8e95adf62918b739fe80da309f2a6c484c6a1269
ynwahid <yusufnurwahid.contact@gmail.com>
2022-02-20 09:18:01 +0700

    remove trx

--- main.go
+++ main.go
@@ -5,24 +5,20 @@ import (
 	"group-project1/deliveries/controllers/address"
 	"group-project1/deliveries/controllers/admin"
 	"group-project1/deliveries/controllers/auth"
-	"group-project1/deliveries/controllers/product"
-	"group-project1/deliveries/controllers/product_category"
-	"group-project1/deliveries/controllers/user"
 	"group-project1/deliveries/controllers/order"
 	"group-project1/deliveries/controllers/payment_method"
-	"group-project1/deliveries/controllers/transaction"
+	"group-project1/deliveries/controllers/product"
+	"group-project1/deliveries/controllers/user"
 	route "group-project1/deliveries/routes"
 	"group-project1/utils"
 
 	_addressRepo "group-project1/repository/address"
 	_adminRepo "group-project1/repository/admin"
 	_authRepo "group-project1/repository/auth"
-	_productRepo "group-project1/repository/product"
-	_productCatRepo "group-project1/repository/product_category"
-	_userRepo "group-project1/repository/user"
 	_orderRepo "group-project1/repository/order"
 	_payRepo "group-project1/repository/payment_method"
-	_transactionRepo "group-project1/repository/transaction"
+	_productRepo "group-project1/repository/product"
+	_userRepo "group-project1/repository/user"
 
 	"fmt"
 
@@ -44,14 +40,12 @@ func main() {
 	adminController := admin.New(adminRepo)
 	productRepo := _productRepo.New(db)
 	productController := product.New(productRepo)
-	productCatRepo := _productCatRepo.New(db)
-	productCatController := product_category.New(productCatRepo)
 	orderRepo := _orderRepo.New(db)
 	orderController := order.New(orderRepo)
 	payMethodRepo := _payRepo.New(db)
 	payMethodController := payment_method.New(payMethodRepo)
-	transactionRepo := _transactionRepo.New(db)
-	transactionController := transaction.New(transactionRepo)
+	// transactionRepo := _transactionRepo.New(db)
+	// transactionController := transaction.New(transactionRepo)
 
 	e := echo.New()
 
@@ -60,10 +54,9 @@ func main() {
 	route.RegisterAddressPath(e, addressController)
 	route.RegisterAdminPath(e, adminController)
 	route.RegisterProductPath(e, productController)
-	route.RegisterProductCatPath(e, productCatController)
 	route.RegisterOrderPath(e, orderController)
 	route.RegisterPayMethodPath(e, payMethodController)
-	route.RegisterTransactionPath(e, transactionController)
-	
+	// route.RegisterTransactionPath(e, transactionController)
+
 	log.Fatal(e.Start(fmt.Sprintf(":%d", config.Port)))
 }
