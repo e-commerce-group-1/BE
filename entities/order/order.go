ac2241eae18330d2589620c5e0138e88c51ed5f2
ynwahid <yusufnurwahid.contact@gmail.com>
2022-02-20 19:46:28 +0700

    add address and order relation

--- entities/order/order.go
+++ entities/order/order.go
@@ -1,6 +1,7 @@
 package order
 
 import (
+	"group-project1/entities/address"
 	"group-project1/entities/transaction"
 
 	"gorm.io/gorm"
@@ -11,5 +12,6 @@ type Orders struct {
 	Phone           string                     `gorm:"type:varchar(13); not null"`
 	Status          bool                       `gorm:"type:boolean"`
 	Transaction     []transaction.Transactions `gorm:"foreignKey:OrderID"`
+	Address         address.Addresses          `gorm:"foreignKey:OrderID"`
 	PaymentMethodID uint
 }
