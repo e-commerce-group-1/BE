c471a44fa6c51b6d2a0e988ab1b68ff547093878
ynwahid <yusufnurwahid.contact@gmail.com>
2022-02-20 08:53:57 +0700

    change size data type

--- entities/product/product.go
+++ entities/product/product.go
@@ -12,7 +12,7 @@ type Products struct {
 	Category          string `gorm:"type:enum('Sneakers','Shirts', 'Pants', 'Accessories')"`
 	Description       string `gorm:"type:text"`
 	Gender            bool   `gorm:"type:boolean"`
-	Size              string `gorm:"type:varchar(255)`
+	Size              string `gorm:"type:varchar(255)"`
 	Price             uint   `gorm:"default:0"`
 	Stock             uint   `gorm:"default:0"`
 	Image             string `gorm:"type:text"`
