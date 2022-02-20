d342e20e4bdcd05093b529aebf8d953b002c4f5f
ynwahid <yusufnurwahid.contact@gmail.com>
2022-02-20 17:26:22 +0700

    fix order entity

--- entities/order/order.go
+++ entities/order/order.go
@@ -1,13 +1,15 @@
 package order
 
 import (
+	"group-project1/entities/transaction"
+
 	"gorm.io/gorm"
 )
 
 type Orders struct {
 	gorm.Model
-	Phone           string `gorm:"type:varchar(13); not null"`
-	Status          bool   `gorm:"type:boolean"`
-	TransactionID   uint
+	Phone           string                     `gorm:"type:varchar(13); not null"`
+	Status          bool                       `gorm:"type:boolean"`
+	Transaction     []transaction.Transactions `gorm:"foreignKey:OrderID"`
 	PaymentMethodID uint
 }
