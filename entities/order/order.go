bcd1f72fe32bb10124330d485268c6446619a646
ynwahid <yusufnurwahid.contact@gmail.com>
2022-02-20 23:09:24 +0700

    delete trx foreign key

--- entities/order/order.go
+++ entities/order/order.go
@@ -2,17 +2,16 @@ package order
 
 import (
 	"group-project1/entities/address"
-	"group-project1/entities/transaction"
 
 	"gorm.io/gorm"
 )
 
 type Orders struct {
 	gorm.Model
-	Phone           string                     `gorm:"type:varchar(13); not null"`
-	Status          bool                       `gorm:"type:boolean"`
-	Transaction     []transaction.Transactions `gorm:"foreignKey:OrderID"`
-	Address         address.Addresses          `gorm:"foreignKey:OrderID"`
+	Phone           string            `gorm:"type:varchar(13); not null"`
+	Status          string            `gorm:"type:enum('order', 'payed', 'cancel');default:'order'"`
+	TransactionID   string            `gorm:"type:text"`
+	Address         address.Addresses `gorm:"foreignKey:OrderID"`
 	PaymentMethodID uint
 	UserID          uint
 }
